01bdbc6387ebe038b97f35a45f2b1fff3ea55bc9
MinhVu <vudau080807@gmail.com>
2025-09-28 14:11:27 +0700

    Initial commit: Hello World with Copilot

--- MyFirstProject/main.cpp
+++ MyFirstProject/main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
   float grade1 = 85.5; 
-  float grade2 = 97.0;
+  float grade2 = 100.0;
   float total, average;
    total = grade1 + grade2;
   average = total/2.0 ;  // divide the total by 2.0
