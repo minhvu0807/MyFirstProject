a1cc796964bd4880b709c247027fe5339d97f9e6
MinhVu <vudau080807@gmail.com>
2025-10-13 10:21:46 +0700

    commit it to github

--- MyFirstProject/main.cpp
+++ MyFirstProject/main.cpp
@@ -1,7 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-  double pi = 3.141592653589 ; 
-  cout<<scientific<<setprecision(4)<<pi<<endl<<-1; 
+  int a , b , c ; 
+  a = 15, b = 17 , c = 33 ; 
+  {
+        int a = 99 ; 
+        int b = 19 ;
+        int c = 9 ;
+        cout<<a<<" "<<b<<" "<<c<<endl ; 
+  }
+  cout<<a<<" "<<c<<" "<<b<<endl ;
   return 0;
 }
